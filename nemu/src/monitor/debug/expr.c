3c26d46571246ce2d69d6519fdce1f462e46b39d
tracer-NEMU2020 <tjuics2020@163.com>
2023-09-07 18:52:32 -0700

    >  compile NEMU 3022244290 trouverecc Linux ubuntu 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:52:32 up 20 min,  1 user,  load average: 1.84, 0.86, 0.48 b7b92346dfb6b1a074fd48d5e149e52955e6c98

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -7,13 +7,13 @@
 #include <regex.h>
 #include <stdlib.h>
 
-uint32_t look_up_symtab(char *, bool *);
+//uint32_t look_up_symtab(char *, bool *);
 
 enum {
 	NOTYPE = 256, EQ
 
 	/* TODO: Add more token types */
-        , NUM, NEQ, OR, AND, REG, REF, NEG, ID
+        , NUM, NEQ, OR, AND, REG, REF, NEG//, ID
 };
 
 static struct rule {
@@ -40,8 +40,8 @@ static struct rule {
 	{"\\|\\|", OR},
 	{"!", '!'},
 	{"\\(", '('},
-	{"\\)", ')'} ,
-	{"[a-zA-Z_]{1,31}", ID},
+	{"\\)", ')'} //,
+	//{"[a-zA-Z_]{1,31}", ID},
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
@@ -98,7 +98,7 @@ static bool make_token(char *e) {
 				switch(rules[i].token_type) {
                                         case NOTYPE: break;
                                         case NUM:
-										case ID:
+										//case ID:
 					//default: panic("please implement me");
                                         case REG: sprintf(tokens[nr_token].str, "%.*s", substr_len, substr_start);
 					default: tokens[nr_token].type = rules[i].token_type;
